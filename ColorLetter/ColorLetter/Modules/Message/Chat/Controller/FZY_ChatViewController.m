5933fd58abbcccf5106f938185af4a2d534138d3
heroGuo <18341894356@163.com>
2016-10-26 15:43:58 +0800

    郭乐峰10.26

--- ColorLetter/ColorLetter/Modules/Message/Chat/Controller/FZY_ChatViewController.m
+++ ColorLetter/ColorLetter/Modules/Message/Chat/Controller/FZY_ChatViewController.m
@@ -16,6 +16,7 @@ UITableViewDataSource
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) UITextField *importTextField;
 @property (nonatomic, strong) UIButton *sendMessageButton;
+@property (nonatomic, strong) NSMutableArray *dataSourceArray;
 @end
 
 @implementation FZY_ChatViewController
@@ -33,14 +34,18 @@ UITableViewDataSource
     self.view.backgroundColor = [UIColor whiteColor];
     self.title = _friendName;
     [self creatChatTableViewAndTextField];
+    
+   // EaseMessageViewController *chatController = [[EaseMessageViewController alloc] initWithConversationChatter:@"8001" conversationType:EMConversationTypeChat];
 }
 
-
 - (void)creatChatTableViewAndTextField {
+    self.dataSourceArray = [NSMutableArray array];
+    
     // 创建 tableView
-    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, WIDTH, HEIGHT) style:UITableViewStylePlain];
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, WIDTH, HEIGHT - 50) style:UITableViewStylePlain];
     _tableView.delegate = self;
     _tableView.dataSource = self;
+
     [self.view addSubview:_tableView];
     [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"chatCell"];
     
@@ -60,14 +65,24 @@ UITableViewDataSource
     [downView addSubview:_sendMessageButton];
     [_sendMessageButton handleControlEvent:UIControlEventTouchUpInside withBlock:^{
         NSLog(@"发送消息");
+        NSString *str = _importTextField.text;
+        [_dataSourceArray addObject:str];
+     //   [_tableView scrollToRowAtIndexPath:_dataSourceArray.count atScrollPosition:UITableViewScrollPositionTop animated:YES];
+        
+        [_tableView reloadData];
     }];
     
 }
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 20;
+    return _dataSourceArray.count;
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"chatCell"];
+    if (_dataSourceArray.count <= 0) {
+        return cell;
+    }
+    cell.textLabel.text = _dataSourceArray[indexPath.row];
     return cell;
 }
 
